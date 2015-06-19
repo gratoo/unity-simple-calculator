ba7c6106d0dea144d3ea0031568bebedfa80ec95
gratoo <gratoo@gmail.com>
2015-06-19 03:06:22 -0700

    basic integration of the leap motion

---
+++ simple-calculator/Assets/_Project/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Calculator _calculator;
+	public string _value;
+
+	private void OnMouseDown(){
+		_calculator.SetInputValue (_value);
+	}
+
+	private void OnTriggerEnter(Collider collider){
+		Transform transform = collider.GetComponent<Transform> ();
+
+		if (transform.name == "bone3" && transform.parent.name == "index") {
+			_calculator.SetInputValue (_value);
+		}
+	}
+}
